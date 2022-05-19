b5bc1e4939d368ffe48c3dfd205f13d1987f9070
abbat01 <95858072+abbat01@users.noreply.github.com>
2022-05-19 12:33:25 +0300

    init: add a code (Thu, 19.05, 12:35:21)

---
+++ 01-read-file/index.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+const path = require('path');
+const stream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf8');
+const { stdout } = require('process');
+stream.on('data', (e) => stdout.write(e));
