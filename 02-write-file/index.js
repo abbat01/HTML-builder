735651e5d9a03d5d69e398da59ffb62572a834f9
abbat01 <95858072+abbat01@users.noreply.github.com>
2022-05-24 21:01:08 +0300

    feat: fix ctrl c (Thu, 24.05 21:03:10)

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,11 +1,14 @@
 const path = require('path');
+const readline = require('readline');
 const fs = require('fs');
 const {
   stdin,
   stdout,
-  exit
-} = require('process');
+  stdin: input,
+  stdout: output,
+  exit} = require('process');
 const stream = fs.createWriteStream(path.join(__dirname, 'result.txt'));
+const rl = readline.createInterface({ input, output });
 stdout.write('Hi, ask your question...\n');
 stdin.on('data', message => {
   if (message.toString().trim() === 'exit') {
@@ -14,7 +17,12 @@ stdin.on('data', message => {
     stream.write(message);
   }
 });
+rl.on('SIGINT', () => {
+  console.log('Goodbye! Exit of program by `Ctrl + C`');
+  rl.close();
+});
 function close () {
   stdout.write('Thanks you, goodbye');
   exit();
 }
+
